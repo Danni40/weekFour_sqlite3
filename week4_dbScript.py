4cf0b7dec9d171395c3b93c4c78e2583ffb69f95
Danni40 <dlewis2@live.maryville.edu>
2018-07-29 14:27:20 -0500

    Script for Alice
    Script to add user, delete alice and reassign her experiment.

---
+++ week4_dbScript.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 29 13:48:09 2018
+
+@author: Danni Lewis
+"""
+#import the module
+import sqlite3
+#connect or create a local db
+connection = sqlite3.connect("week4.db")
+#run connection functions
+cursor = connection.cursor()
+#insert more experiment assignments
+sql_command = """
+INSERT INTO experiment values ( Null, 'EBV Vaccine trial', 1, 'A vaccine trial');
+"""
+#execute the sql command
+cursor.execute(sql_command)
+sql_command = """
+INSERT INTO experiment values ( Null, 'Flu Antibody Study', 3, 'Study of the morphology of flu antibodies');
+"""
+cursor.execute(sql_command)
+sql_command = """
+INSERT INTO experiment values ( Null, 'EBV Vaccine trial', 5, 'Study of the morphology of flu antibodies');
+"""
+cursor.execute(sql_command)
+sql_command = """
+INSERT INTO experiment values ( Null, 'Flu Antibody Study', 6, 'Study of the morphology of flu antibodies');
+"""
+cursor.execute(sql_command)
+#add new user
+sql_command = """
+INSERT INTO people values ( Null, 'Danni', 'Research Executive', '555-123-1000', '35F');
+"""
+#execute the sql command
+cursor.execute(sql_command)
+#delete Alice
+sql_command = """
+DELETE FROM people where name="Alice";
+"""
+cursor.execute(sql_command)
+#assign Alice's assignments to the new user
+sql_command = """
+UPDATE experiment set name='EBV Flu Antibody Study' where id=6;
+"""
+cursor.execute(sql_command)
+#commit all sql commands
+connection.commit()
+connection.close()
